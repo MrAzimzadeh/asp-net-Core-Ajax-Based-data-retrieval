8cdd3339cda6ad5dcef6e73cf692e0ed95458bbb
MrAzimzada <mehemmedezimzase@gmail.com>
2023-03-25 20:58:04 +0400

    Ajax

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using AjaxBasedDataRetrieval.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AjaxBasedDataRetrieval.Controllers
+{
+    public class AjaxData
+    {
+        public string A { get; set; }
+        public string B { get; set; }
+
+    }
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult GetProducts()
+        {
+            return View();
+        }
+
+        public IActionResult CreateProducts()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult VeriAl(AjaxData ajaxData)
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AjaxBasedDataRetrieval.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
